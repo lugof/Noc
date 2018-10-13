a2b20ed973bbc021385ea970ca431f62d041d5e4
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-13 01:13:36 -0500

    manual wid

--- public/client.js
+++ public/client.js
@@ -6,7 +6,7 @@ $(function(){
   var socket = io.connect();
   socket.on('stream', function(tweet) {
 
-   $('body').append('<div class="tweet">' + tweet + '</div>')
+  // $('body').append('<div class="tweet">' + tweet + '</div>')
    arr2.push(tweet);
    console.log(arr2);
     
