97d485a940d727b7630b8e72bef23c76c5919cf1
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-12 21:35:21 -0500

    manual wid

--- public/client.js
+++ public/client.js
@@ -4,14 +4,42 @@ $(function(){
   var socket = io.connect();
   socket.on('stream', function(tweet) {
     
+    twttr.ready( function (twttr) {
+      twttr.widgets.createTweet.then(
+        tweet,
+        document.getElementById('container')
+        
+        
+        
+      ).then( function( el ) {
+    console.log('Tweet added.');
 
-
-    $('body').append('<div class="tweet">' + tweet + '</div>');
+    
 
 arr2.push(tweet);
 console.log(arr2);
 
-    
+      
 
 });
 });
+  });
+})
+
+/*
+$('body').append('<div class="tweet">' + tweet + '</div>');
+
+twttr.ready( function (twttr) {
+  twttr.widgets.createTweet.then(
+    tweet,
+    document.getElementById('container')
+    
+    
+    
+  ).then( function( el ) {
+console.log('Tweet added.');
+}
+);
+});
+*/
+ 
