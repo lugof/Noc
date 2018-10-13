9687f15f18cb557329c673ed0c707980f05955f9
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-13 01:30:57 -0500

    manual wid

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ io.sockets.on('connection', function (socket) {
 
   stream.on('tweet', function (tweet) {
 
-    io.sockets.emit('stream',tweet.id);
-    console.log(tweet.id);
+    io.sockets.emit('stream',tweet.id_str);
+    console.log(tweet.id_str);
 
   });
  });
