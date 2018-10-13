151c0ffb862283087fdf5b389f0b259f72ea0922
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-12 19:33:46 -0500

    showing embbed tweets by id

--- public/client.js
+++ public/client.js
@@ -1,7 +1,22 @@
 $(function(){
   var socket = io.connect();
   socket.on('stream', function(tweet) {
-    console.log(tweet);
-    $('body').append('<div class="tweet">' + tweet + '</div>');
+    twttr.ready( function (twttr) {
+      twttr.widgets.createTweet(
+        tweet,
+        document.getElementById('container')
+        
+        
+        
+      ).then( function( el ) {
+  console.log('Tweet added.');
+  }
+);
+});
+
+
+
+   // console.log(tweet);
+    //$('body').append('<div class="tweet">' + tweet + '</div>');
 });
 });
