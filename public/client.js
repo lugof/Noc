f62972eb4ebdd3badfa0f74c64a5bf0989c118f9
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-13 00:15:18 -0500

    manual wid

--- public/client.js
+++ public/client.js
@@ -1,4 +1,19 @@
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t; 
+}(document, "script", "twitter-wjs"));
 
 
 
@@ -6,10 +21,28 @@ $(function(){
   var socket = io.connect();
   socket.on('stream', function(tweet) {
 
-   $('body').append('<div class="tweet">' + tweet + '</div>')
-   arr2.push(tweet);
     
-});
+      twttr.widgets.createTweet(
+        tweet,
+        document.getElementById('container')
+        
+        
+        
+      ).then( function( el ) {
+        twttr.widgets.load(
+          document.getElementById("container")
+        );
+      }
+      );
+      });
+    
+
+
+
+
+   //$('body').append('<div class="tweet">' + tweet + '</div>')
+  
+    
 });
   
 
