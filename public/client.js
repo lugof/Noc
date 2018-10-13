5c9d3476f12a48fa793375c1bc3d89ecfe061ede
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-13 01:34:43 -0500

    manual wid

--- public/client.js
+++ public/client.js
@@ -5,10 +5,11 @@
 $(function(){
   var socket = io.connect();
   socket.on('stream', function(tweet) {
+    
 
   // $('body').append('<div class="tweet">' + tweet + '</div>')
-   arr2.push(tweet);
-   console.log(arr2);
+   //arr2.push(tweet);
+   console.log(tweet);
     
 });
 });
